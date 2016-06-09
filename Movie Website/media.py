93c3e60ed0cfffcb4ac8ab2d6398a1b468f5e27b
Jai Kathuria <jaikathuria@live.com>
2016-06-09 14:09:03 +0530

    Second Upload
    Made the changes as per request

--- Movie Website/media.py
+++ Movie Website/media.py
@@ -1,8 +1,19 @@
 import webbrowser
 class Movie():
-    VALID_RATINGS = ["G","PG","PG-13","R"]
+    """
+    this class defines the basic structure of Movie objects.
+    """
     
     def __init__(self, movie_title, movie_storyline, movie_rating, poster_image, youtube_trailer):
+        """
+        this constructor method takes 5 strings as input 
+        1 --> Movie Title
+        2 --> Storyline
+        3 --> IMDB rating
+        4 --> Link to poster Image
+        5 --> Link to youtube trailer
+        this function does not return anything
+        """
         self.title = movie_title
         self.storyline = movie_storyline
         self.rating = movie_rating
@@ -10,5 +21,8 @@ class Movie():
         self.trailer_youtube_url = youtube_trailer
     
     def show_trailer(self):
+        """
+        This fuction does not take any input and opens youtube trailer of the movie instance for which this fuction is called.
+        """
         webbrowser.open(self.trailer_youtube_url)
         
